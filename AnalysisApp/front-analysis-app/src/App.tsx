06e8b798023c9aae3877803a5effa5f1eb4a99c1
Masahiro Yamada <myamagata504ninja@gmail.com>
2024-07-14 20:28:26 +0900

    reactディレクトリを追加

---
+++ AnalysisApp/front-analysis-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello React!</h1>
+      <p>aaa</p>
+    </div>
+  )
+}
+
+export default App
