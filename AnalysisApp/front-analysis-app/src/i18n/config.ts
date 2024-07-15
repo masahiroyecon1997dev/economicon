d0bbee5f183b0f7b38311d61f4c8f9bc45a064ca
Masahiro Yamada <myamagata504ninja@gmail.com>
2024-07-15 15:25:21 +0900

    ヘッダーメニュー作成中

---
+++ AnalysisApp/front-analysis-app/src/i18n/config.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+// 言語jsonファイルのimport
+import translation_en from "./locales/en.json";
+import translation_ja from "./locales/ja.json";
+
+const resources = {
+    ja: {
+      translation: translation_ja
+    },
+    en: {
+      translation: translation_en
+    }
+  };
+
+  i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "ja",
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+  export default i18n;
