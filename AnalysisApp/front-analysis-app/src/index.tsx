7bf3a10cc369a41402c19d3d79ea134dcb56d010
masahiro <myamagata504ninja@gmail.com>
2024-07-21 15:20:41 +0900

    ファイル操作作成中

--- AnalysisApp/front-analysis-app/src/index.tsx
+++ AnalysisApp/front-analysis-app/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./i18n/config";
 import './index.css';
-import App from './components/templates/App';
+import { App } from './components/templates/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
