7e8d94b202db1f08f9bff5125da4f565209c2f11
Masahiro Yamada <myamagata504ninja@gmail.com>
2024-08-03 13:32:11 +0900

    取り込みモーダル作成中

--- AnalysisApp/front-analysis-app/src/components/molecules/HeaderMenuDropdown/HeaderMenuDropdown.tsx
+++ AnalysisApp/front-analysis-app/src/components/molecules/HeaderMenuDropdown/HeaderMenuDropdown.tsx
@@ -12,19 +12,27 @@ export function HeaderMenuDropdown({ children, dropdownListElement }: HeaderMenu
   const [openDropdown, setOpenDropdown] = useState<boolean>(false);
 
   function clickMenu() {
-    setOpenDropdown((preOpenDropdown) => (!preOpenDropdown))
+    setOpenDropdown((preOpenDropdown) => (!preOpenDropdown));
+  }
+
+  function onMouseEvent() {
+    setOpenDropdown(true);
+  }
+
+  function leaveMouseEvent() {
+    setOpenDropdown(false);
   }
 
   return (
-    <>
+    <div onMouseEnter={() => onMouseEvent()} onMouseLeave={() => leaveMouseEvent()}>
       <HeaderMenuButton clickEvent={() => clickMenu()}>{children}</HeaderMenuButton>
         {openDropdown && (
-          <div className="absolute bg-white shadow-md border mt-1 rounded-md">
+          <div className="absolute bg-white shadow-md border rounded-md">
             {dropdownListElement.map((item, i) => (
               <HeaderMenuDropdownButton key={i} clickEvent={item.dropdownListFunction}>{item.dropdownListName}</HeaderMenuDropdownButton>
             ))}
           </div>
         )}
-    </>
+    </div>
   )
 }
