b039451a6cc38e00e564502334df646c8342859e
Masahiro Yamada <myamagata504ninja@gmail.com>
2024-09-16 14:20:46 +0900

    モーダルアニメーション

---
+++ AnalysisApp/front-analysis-app/src/components/atoms/InputText/InputText.tsx
@@ -0,0 +1,20 @@
+import React, { ChangeEvent } from "react";
+
+type InputTextProps = {
+  value: string;
+  change: (event: ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
+};
+
+export function InputText({ value, change, error }: InputTextProps) {
+  return (
+    <input
+      type="text"
+      className={`border ${
+        error ? "border-red-300" : "border-gray-300"
+      } block w-full max-w-xs px-4 py-1 text-sm font-normal shadow-xs text-gray-900 bg-transparent rounded-md placeholder-gray-400 focus:outline-none leading-relaxed`}
+      value={value}
+      onChange={(event) => change(event)}
+    />
+  );
+}
