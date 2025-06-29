403385e276ba1aef88f888bddd16d3fc1914f71d
Masahiro Yamada <myamagata504ninja@gmail.com>
2025-06-29 20:00:44 +0900

    バリデーション作成、列追加API作成

--- AnalysisApp/ApiServer/analysisapp/api/apis/utilities/validator/common_validators.py
+++ AnalysisApp/ApiServer/analysisapp/api/apis/utilities/validator/common_validators.py
@@ -1,40 +1,42 @@
 from pathlib import Path
 from typing import Optional, List, Union
-from rest_framework import status
 from django.utils.translation import gettext as _
-from ..create_response import create_error_response
+from rest_framework import status
+
+
+class CommonValidationError(Exception):
+    """
+    入力バリデーション専用の例外
+    """
+    def __init__(self,
+                 message: str,
+                 status_code: int = status.HTTP_400_BAD_REQUEST):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(message)
 
 
 def validate_required(value: any, param_name: str) -> None:
     """パラメータが空かどうかをチェック"""
     if value is None or not value:
-        messege = _(f"{param_name} is required.")
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                messege
-            )
+        message = _(f"{param_name} is required.")
+        raise CommonValidationError(message)
     return None
 
 
 def validate_number(value: any, param_name: str) -> None:
     """パラメータが数値どうかをチェック"""
     if (not isinstance(value, (int, float))):
-        messege = _(f"{param_name} must be a number.")
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                messege
-            )
+        message = _(f"{param_name} must be a number.")
+        raise CommonValidationError(message)
     return None
 
 
 def validate_required_string(value: any, param_name: str) -> None:
     """パラメータが文字列どうかをチェック"""
     if (isinstance(value, str) and not value.strip()):
-        messege = _(f"{param_name} mustbe a string.")
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                messege
-            )
+        message = _(f"{param_name} mustbe a string.")
+        raise CommonValidationError(message)
     return None
 
 
@@ -46,17 +48,11 @@ def validate_string_length(value: any,
     if min_length is not None and len(value) < min_length:
         message = _(f"{param_name} must be at {min_length} to {max_length} "
                     f"characters long.")
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                message
-            )
+        raise CommonValidationError(message)
     if max_length is not None and len(value) > max_length:
         message = _(f"{param_name} must be at {min_length} to {max_length} "
                     f"characters long.")
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                message
-            )
+        raise CommonValidationError(message)
     return None
 
 
@@ -66,10 +62,7 @@ def validate_invalid_chars(value: str, param_name: str,
     message = _(f"{param_name} contains invalid characters.")
     for char in invalid_chars:
         if char in value:
-            raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                message
-            )
+            raise CommonValidationError(message)
     return None
 
 
@@ -79,15 +72,13 @@ def validate_numeric_range(value: Union[int, float], param_name: str,
                            ) -> None:
     """数値の範囲をチェック"""
     if min_value is not None and value < min_value:
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                f"{param_name} must be between {min_value} and {max_value}."
-            )
+        message = _(f"{param_name} must be between {min_value} "
+                    "and {max_value}.")
+        raise CommonValidationError(message)
     if max_value is not None and value > max_value:
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                f"{param_name} must be between {min_value} and {max_value}."
-            )
+        message = _(f"{param_name} must be between {min_value} "
+                    "and {max_value}.")
+        raise CommonValidationError(message)
     return None
 
 
@@ -95,44 +86,62 @@ def validate_enum(value: any, param_name: str,
                   allowed_values: List[any]) -> None:
     """列挙型の値をチェック"""
     if value not in allowed_values:
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                f"{param_name} is invalid value."
-            )
+        message = _(f"{param_name} is invalid value.")
+        raise CommonValidationError(message)
     return None
 
 
 def validate_path(value: str, param_name: str) -> None:
     """パスのバリデーション"""
     # 空のパスをチェック
+    message = _(f"{param_name} is required.")
     if validate_required(value, param_name):
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                f"{param_name} is required."
-            )
+        raise CommonValidationError(message)
     # 危険な文字列をチェック
     dangerous_patterns = ['..', '~', '$', '|', ';', '&', '`']
     if any(pattern in value for pattern in dangerous_patterns):
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                f"{param_name} is required."
-            )
+        raise CommonValidationError(message)
     # 絶対パスかどうかをチェック
     try:
         Path(value).resolve()
     except (OSError, ValueError):
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                f"{param_name} is required."
-            )
+        raise CommonValidationError(message)
     return None
 
 
-def validate_table_exists(value: str, tables: dict) -> None:
+def validate_table_exists(value: str, param_name: str, tables: dict) -> None:
     """テーブルが存在するかチェック"""
     if value not in tables:
-        raise create_error_response(
-                status.HTTP_400_BAD_REQUEST,
-                f"The table '{value}' does not exist."
-            )
+        message = _(f"{param_name} '{value}' does not exist.")
+        raise CommonValidationError(message)
+    return None
+
+
+def validate_table_duplicate(value: str,
+                             param_name: str,
+                             tables: dict) -> None:
+    """テーブルが重複していないかチェック"""
+    if value in tables:
+        message = _(f"{param_name} '{value}' already exists.")
+        raise CommonValidationError(message)
+    return None
+
+
+def validate_column_exists(value: str,
+                           param_name: str,
+                           columns: List[str]) -> None:
+    """列が存在するかチェック"""
+    if value not in columns:
+        message = _(f"{param_name} '{value}' does not exist.")
+        raise CommonValidationError(message)
+    return None
+
+
+def validate_column_duplicate(value: str,
+                              param_name: str,
+                              columns: List[str]) -> None:
+    """列が重複していないかチェック"""
+    message = _(f"{param_name} '{value}' already exists.")
+    if value in columns:
+        raise CommonValidationError(message)
     return None
