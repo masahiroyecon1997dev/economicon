1fe76e8c5bc8ac803ef81fc9cde4ee9708b5e0a3
Masahiro Yamada <myamagata504ninja@gmail.com>
2025-07-06 17:24:40 +0900

    テーブル・列作成、名前変更、削除

--- AnalysisApp/ApiServer/analysisapp/api/apis/create_table.py
+++ AnalysisApp/ApiServer/analysisapp/api/apis/create_table.py
@@ -9,7 +9,7 @@ from .utilities.create_log import create_log_api_request
 from .utilities.validator.input_request_validators import (
     validate_create_table_request,
 )
-from .data.tables_info import all_tables_info
+from .data.tables_info import (TableInfo, all_tables_info)
 
 
 class CreateTable(APIView):
@@ -18,19 +18,39 @@ class CreateTable(APIView):
         try:
             # リクエスト受け取りログ
             create_log_api_request(request)
+            # リクエストデータの取得
+            request_data = json.loads(request.body)
             # バリデーション
-            validation_error = validate_create_table_request(request)
+            validation_error = validate_create_table_request(request_data)
             if validation_error:
                 return validation_error
 
-        data = {}
-        df = pl.DataFrame(data)
+            table_name = request_data['tableName']
+            table_number_of_rows = request_data['tableNumberOfRows']
+            new_columns_name = request_data['columns']
 
+            new_column_data_none = [None] * table_number_of_rows
+
+            data = {}
+            for column_name in new_columns_name:
+                data[column_name] = new_column_data_none
+
+            df = pl.DataFrame(data)
+            table_info = TableInfo(
+                table_name=table_name,
+                table=df
+            )
+            all_tables_info[table_name] = table_info
+            result = {'tableName': table_name}
+            return create_success_response(
+                status.HTTP_200_OK,
+                result,
+            )
         except Exception as e:
             message = _("An unexpected error occurred during "
                         "adding column processing")
             return create_error_response(
-                status.HTTP_400_BAD_REQUEST,
+                status.HTTP_500_INTERNAL_SERVER_ERROR,
                 message,
                 e
             )
