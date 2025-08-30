737509872712c35cced1517fd9b829d91e66b6a4
Masahiro Yamada <myamagata504ninja@gmail.com>
2025-08-30 20:59:11 +0900

    ユニオンテーブル作成API調整

--- AnalysisApp/ApiServer/analysisapp/api/tests/test_create_table.py
+++ AnalysisApp/ApiServer/analysisapp/api/tests/test_create_table.py
@@ -85,4 +85,4 @@ class TestApiCreateTable(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response_data['code'], 'NG')
         self.assertIn(response_data['message'],
-                      "columnNames must be with at least one item.")
+                      "columnNames must be with at least 1 item.")
