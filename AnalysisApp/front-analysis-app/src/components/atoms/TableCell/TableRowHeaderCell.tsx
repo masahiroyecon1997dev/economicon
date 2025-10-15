8ccdb5c3cd5a66743ea5f3d5567356532197db00
MasahiroYamada1997-1 <myamagata504ninja@gmail.com>
2025-10-15 22:59:41 +0900

    stitchのスタイルの反映完了

--- AnalysisApp/front-analysis-app/src/components/atoms/TableCell/TableRowHeaderCell.tsx
+++ AnalysisApp/front-analysis-app/src/components/atoms/TableCell/TableRowHeaderCell.tsx
@@ -9,7 +9,7 @@ type TableRowHeaderCellProps = {
 export function TableRowHeaderCell({ children, onContextMenu }: TableRowHeaderCellProps) {
   return (
     <th
-      className="px-4 py-2 bg-gray-200 border-b border-r text-left cursor-grab"
+      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
       onContextMenu={onContextMenu}
     >
       {children}
