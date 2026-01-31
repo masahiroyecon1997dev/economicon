f71ea38bb37d834baf0fb95197553879e18965f3
Masahiro Yamada <myamagata504ninja@gmail.com>
2026-01-31 23:39:19 +0900

    バイナリでテーブルのデータ取得

--- ApiServer/analysisapp/schemas/__init__.py
+++ ApiServer/analysisapp/schemas/__init__.py
@@ -1,70 +1,110 @@
 """スキーマパッケージ初期化"""
-from .columns import (AddColumnRequest, AddDummyColumnRequest,
-                      AddLagLeadColumnRequest, AddSimulationColumnRequest,
-                      CalculateColumnRequest, DeleteColumnRequest,
-                      DuplicateColumnRequest, GetColumnListRequest,
-                      RenameColumnRequest, SortColumnsRequest,
-                      TransformColumnRequest)
+
+from .columns import (
+    AddColumnRequest,
+    AddDummyColumnRequest,
+    AddLagLeadColumnRequest,
+    AddSimulationColumnRequest,
+    CalculateColumnRequest,
+    DeleteColumnRequest,
+    DuplicateColumnRequest,
+    GetColumnListRequest,
+    RenameColumnRequest,
+    SortColumnsRequest,
+    TransformColumnRequest,
+)
 from .common import BaseResponse, ErrorResponse, SuccessResponse
-from .data_io import (ExportCsvByPathRequest, ExportExcelByPathRequest,
-                      ExportParquetByPathRequest, ImportCsvByPathRequest,
-                      ImportExcelByPathRequest, ImportParquetByPathRequest)
+from .data_io import (
+    ExportCsvByPathRequest,
+    ExportExcelByPathRequest,
+    ExportParquetByPathRequest,
+    ImportCsvByPathRequest,
+    ImportExcelByPathRequest,
+    ImportParquetByPathRequest,
+)
 from .files import GetFilesRequest
 from .operations import FilterSingleConditionRequest, InputCellDataRequest
 from .regressions import RegressionRequest
 from .settings import GetSettingsRequest
 from .statistics import ConfidenceIntervalRequest, DescriptiveStatisticsRequest
-from .tables import (ClearTablesRequest, CreateJoinTableRequest,
-                     CreateSimulationDataTableRequest, CreateTableRequest,
-                     CreateUnionTableRequest, DeleteTableRequest,
-                     DuplicateTableRequest, FetchDataToJsonRequest,
-                     GetTableListRequest, RenameTableRequest)
+from .tables import (
+    ClearTablesRequest,
+    CreateJoinTableRequest,
+    CreateSimulationDataTableRequest,
+    CreateTableRequest,
+    CreateUnionTableRequest,
+    DeleteTableRequest,
+    DuplicateTableRequest,
+    FetchDataToArrowRequest,
+    FetchDataToJsonRequest,
+    GetTableListRequest,
+    RenameTableRequest,
+)
 
 __all__ = [
     # Common
-    'BaseResponse',
-    'SuccessResponse',
-    'ErrorResponse',
+    "BaseResponse",
+    "SuccessResponse",
+    "ErrorResponse",
     # Column operations
-    'AddColumnRequest',
-    'DeleteColumnRequest',
-    'RenameColumnRequest',
-    'DuplicateColumnRequest',
-    'CalculateColumnRequest',
-    'AddDummyColumnRequest',
-    'AddLagLeadColumnRequest',
-    'AddSimulationColumnRequest',
-    'SortColumnsRequest',
-    'TransformColumnRequest',
-    'GetColumnListRequest',
+    "AddColumnRequest",
+    "DeleteColumnRequest",
+    "RenameColumnRequest",
+    "DuplicateColumnRequest",
+    "CalculateColumnRequest",
+    "AddDummyColumnRequest",
+    "AddLagLeadColumnRequest",
+    "AddSimulationColumnRequest",
+    "SortColumnsRequest",
+    "TransformColumnRequest",
+    "GetColumnListRequest",
     # Table operations
-    'CreateTableRequest',
-    'RenameTableRequest',
-    'CreateSimulationDataTableRequest',
-    'CreateJoinTableRequest',
-    'CreateUnionTableRequest',
-    'ClearTablesRequest',
-    'DuplicateTableRequest',
-    'DeleteTableRequest',
-    'FetchDataToJsonRequest',
-    'GetTableListRequest',
+    "CreateTableRequest",
+    "RenameTableRequest",
+    "CreateSimulationDataTableRequest",
+    "CreateJoinTableRequest",
+    "CreateUnionTableRequest",
+    "ClearTablesRequest",
+    "DuplicateTableRequest",
+    "DeleteTableRequest",
+    "FetchDataToJsonRequest",
+    "FetchDataToArrowRequest",
+    "GetTableListRequest",
     # Statistics
-    'ConfidenceIntervalRequest',
-    'DescriptiveStatisticsRequest',
+    "ConfidenceIntervalRequest",
+    "DescriptiveStatisticsRequest",
     # Settings
-    'GetSettingsRequest',
+    "GetSettingsRequest",
     # Files
-    'GetFilesRequest',
+    "GetFilesRequest",
     # Data I/O
-    'ImportCsvByPathRequest',
-    'ImportExcelByPathRequest',
-    'ImportParquetByPathRequest',
-    'ExportCsvByPathRequest',
-    'ExportExcelByPathRequest',
-    'ExportParquetByPathRequest',
+    "ImportCsvByPathRequest",
+    "ImportExcelByPathRequest",
+    "ImportParquetByPathRequest",
+    "ExportCsvByPathRequest",
+    "ExportExcelByPathRequest",
+    "ExportParquetByPathRequest",
     # Regression
-    'RegressionRequest',
+    "RegressionRequest",
     # Operations
-    'InputCellDataRequest',
-    'FilterSingleConditionRequest'
+    "InputCellDataRequest",
+    "FilterSingleConditionRequest",
+    "ConfidenceIntervalRequest",
+    "DescriptiveStatisticsRequest",
+    # Settings
+    "GetSettingsRequest",
+    # Files
+    "GetFilesRequest",
+    # Data I/O
+    "ImportCsvByPathRequest",
+    "ImportExcelByPathRequest",
+    "ImportParquetByPathRequest",
+    "ExportCsvByPathRequest",
+    "ExportExcelByPathRequest",
+    "ExportParquetByPathRequest",
+    # Regression
+    "RegressionRequest",
+    # Operations
+    "InputCellDataRequest",
+    "FilterSingleConditionRequest",
 ]
