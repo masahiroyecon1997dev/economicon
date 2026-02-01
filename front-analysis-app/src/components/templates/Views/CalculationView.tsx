1f124f5d9f1c2b34fd0890875e0edc04070d6caf
Masahiro Yamada <myamagata504ninja@gmail.com>
2026-02-01 09:46:36 +0900

    axios削除、仮想スクロール導入中

--- front-analysis-app/src/components/templates/Views/CalculationView.tsx
+++ front-analysis-app/src/components/templates/Views/CalculationView.tsx
@@ -1,5 +1,4 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios";
 import { CirclePlus, Columns3, Eraser, Info } from "lucide-react";
 import { startTransition, useActionState, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -87,11 +86,8 @@ export const CalculationView = () => {
       }
     } catch (error) {
       let errorMessage = t("Error.UnexpectedError");
-      if (axios.isAxiosError(error)) {
-        const serverMessage = error.response?.data?.message;
-        if (serverMessage) {
-          errorMessage = serverMessage;
-        }
+      if (error instanceof Error) {
+        errorMessage = error.message;
       }
       await showMessageDialog(t("Error.Error"), errorMessage);
       return { success: false };
