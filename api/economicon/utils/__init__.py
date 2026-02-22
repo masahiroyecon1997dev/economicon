9bf02a49f4f19fe8f5e01fb9429f3cc8561d0115
Masahiro Yamada <myamagata504ninja@gmail.com>
2026-02-22 14:45:27 +0900

    ログ修正

--- api/economicon/utils/__init__.py
+++ api/economicon/utils/__init__.py
@@ -2,10 +2,10 @@
 
 from .exceptions import ProcessingError, ValidationError
 from .logging import (
-    create_log_api_error,
-    create_log_api_exception,
-    create_log_api_request,
-    create_log_api_success,
+    log_error,
+    log_exception,
+    log_request,
+    log_success,
 )
 from .response import (
     create_error_response,
@@ -17,10 +17,10 @@ __all__ = [
     "create_success_response",
     "create_success_binary_response",
     "create_error_response",
-    "create_log_api_request",
-    "create_log_api_success",
-    "create_log_api_error",
-    "create_log_api_exception",
     "ValidationError",
     "ProcessingError",
+    "log_error",
+    "log_exception",
+    "log_request",
+    "log_success",
 ]
