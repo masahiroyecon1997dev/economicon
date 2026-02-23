59c5d3703e301b3b8d420801cba5e3b36a7e5f4b
Masahiro Yamada <myamagata504ninja@gmail.com>
2026-02-23 09:42:53 +0900

    ログ修正コミット漏れ＆Ruff準拠修正

--- api/economicon/utils/__init__.py
+++ api/economicon/utils/__init__.py
@@ -2,11 +2,12 @@
 
 from .exceptions import ProcessingError, ValidationError
 from .logging import (
-    configure_file_logging,
     log_error,
     log_exception,
+    log_manager,
     log_request,
     log_success,
+    logger,
 )
 from .response import (
     create_error_response,
@@ -18,7 +19,8 @@ __all__ = [
     "create_success_response",
     "create_success_binary_response",
     "create_error_response",
-    "configure_file_logging",
+    "log_manager",
+    "logger",
     "ValidationError",
     "ProcessingError",
     "log_error",
