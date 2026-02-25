b90206d51d5f6d09ecd8d2d66a752d4f4ab571cf
Masahiro Yamada <myamagata504ninja@gmail.com>
2026-02-25 13:26:44 +0900

    バリデーションメッセージの修正

--- api/economicon/core/messages.py
+++ api/economicon/core/messages.py
@@ -1,29 +1,22 @@
-from economicon.i18n import _  # i18nにある翻訳関数をインポート
+from economicon.i18n import _
 
-# Pydanticエラータイプと日本語テンプレートのマッピング
 VALIDATION_ERROR_TEMPLATES = {
-    "missing": _("{field}は必須項目です。"),
-    "string_type": _("{field}は文字列で入力してください。"),
-    "int_type": _("{field}は整数で入力してください。"),
-    "float_type": _("{field}は数値で入力してください。"),
-    "bool_type": _("{field}は真偽値で入力してください。"),
-    "list_type": _("{field}はリスト形式で入力してください。"),
-    "dict_type": _("{field}は辞書形式で入力してください。"),
-    "string_too_short": _("{field}は{min_length}文字以上で入力してください。"),
-    "string_too_long": _("{field}は{max_length}文字以内で入力してください。"),
-    "too_short": _("{field}は{min_length}件以上ある必要があります。"),
-    "less_than": _("{field}は{lt}未満で入力してください。"),
-    "less_than_equal": _("{field}は{le}以下で入力してください。"),
-    "greater_than": _("{field}は{gt}より大きい値で入力してください。"),
-    "greater_than_equal": _("{field}は{ge}以上で入力してください。"),
-    "literal_error": _(
-        "{field}は次のいずれかである必要があります: {expected}"
-    ),
-    "union_tag_not_found": _(
-        "{field}のタイプ（method等）が正しくありません。"
-    ),
-    "string_pattern_mismatch": _(
-        "{field}に使用できない文字が含まれています。"
-    ),
+    "missing": _("{field} is required"),
+    "string_type": _("{field} must be a string"),
+    "int_type": _("{field} must be an integer"),
+    "float_type": _("{field} must be a number"),
+    "bool_type": _("{field} must be a boolean"),
+    "list_type": _("{field} must be a list"),
+    "dict_type": _("{field} must be a dictionary"),
+    "string_too_short": _("{field} must be at least {min_length} characters"),
+    "string_too_long": _("{field} must be at most {max_length} characters"),
+    "too_short": _("{field} must have at least {min_length} items"),
+    "less_than": _("{field} must be less than {lt}"),
+    "less_than_equal": _("{field} must be less than or equal to {le}"),
+    "greater_than": _("{field} must be greater than {gt}"),
+    "greater_than_equal": _("{field} must be greater than or equal to {ge}"),
+    "literal_error": _("{field} must be one of: {expected}"),
+    "union_tag_not_found": _("{field} has an invalid type"),
+    "string_pattern_mismatch": _("{field} contains invalid characters"),
     "value_error": "{msg}",  # カスタムバリデータのメッセージをそのまま使う
 }
