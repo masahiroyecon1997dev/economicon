7ee4534d2f84f9172be0281cfd78641597226233
masahiroyecon1997dev <107479994+masahiroyecon1997dev@users.noreply.github.com>
2026-06-14 13:16:05 +0900

    信頼区間のタイトルと説明を復活

--- app/src/components/pages/ConfidenceIntervalView.tsx
+++ app/src/components/pages/ConfidenceIntervalView.tsx
@@ -1,6 +1,7 @@
 import { ConfidenceIntervalForm } from "@/components/organisms/Form/ConfidenceIntervalForm";
 import { PageLayout } from "@/components/templates/PageLayout";
 import { useWorkspaceTabsStore } from "@/stores/workspaceTabs";
+import { useTranslation } from "react-i18next";
 
 type ConfidenceIntervalViewProps = {
   className?: string;
@@ -9,10 +10,14 @@ type ConfidenceIntervalViewProps = {
 export const ConfidenceIntervalView = ({
   className: _className,
 }: ConfidenceIntervalViewProps) => {
+  const { t } = useTranslation();
   const closeActiveWorkTab = useWorkspaceTabsStore((s) => s.closeActiveWorkTab);
 
   return (
-    <PageLayout>
+    <PageLayout
+      title={t("ConfidenceIntervalView.Title")}
+      description={t("ConfidenceIntervalView.Description")}
+    >
       <ConfidenceIntervalForm onCancel={closeActiveWorkTab} />
     </PageLayout>
   );
